72f46c23a1e5145ac80ec2a745f6e83545ddf11a
mozart <mozart@mozart-VirtualBox.(none)>
2012-11-06 14:59:40 +0900

    add index.html

---
+++ send.a.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+	<form action='send.b.php' mothed='post'>
+	Data Value send <input name='send' type='text'>
+	<input type='submit'>
+</form>
+</body>
+</html>
---
+++ send.b.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+	
+	<?php echo $_post['send']; ?>
+
+</body>
+</html>
